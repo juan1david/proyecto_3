2561b64e2ddf5817b76b1701e16390b5734faf80
juan1david <juancr200502@gmail.com>
2024-01-18 09:04:01 -0500

    subir repositorio

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
-// Paquete importado
+// Paquete importado para dar formato de colores a la consola.
 require('colors');
 
-// Referencia al módulo math a través de la constante math
+// Referencia al módulo math a través de la constante math.
 const math = require("./math.js");
 
+// Función principal del programa.
 const main = async () => {
+    // Limpia la consola.
     console.clear();
+
+    // Genera dos números enteros aleatorios entre 0 y 10.
     let a = math.getRandomInt(10);
     let b = math.getRandomInt(10);
 
+    // Imprime un encabezado con formato de colores en la consola.
     console.log('***************************************'.blue);
     console.log('* '.blue, 'funciones matematicas'.bgBlue, '*'.blue);
     console.log('***************************************\n'.blue);
+
+    // Imprime un encabezado adicional con formato de colores y muestra los números aleatorios generados.
     console.log('***************************************'.red);
     console.log('  numeros aleatorios:', a, b);
     console.log('*******************************************\n'.blue);
 
+    // Imprime los resultados de las operaciones matemáticas con formato de colores.
     console.log('*******************************************'.blue);
     console.log('**'.blue + '                                 ' + '      **'.blue);
     console.log('**'.blue + ' add:' + math.add(a, b) + '  ' +'                               **'.blue);
@@ -25,4 +33,5 @@ const main = async () => {
     console.log('*******************************************\n'.blue);
 };
 
+// Llama a la función principal para iniciar el programa.
 main();
--- math.js
+++ math.js
@@ -1,21 +1,26 @@
 // math.js
 
+// Función que devuelve un número entero aleatorio hasta el valor máximo especificado.
 const getRandomInt = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
 };
 
+// Función que suma dos números.
 const add = (a, b) => {
     return a + b;
 };
 
+// Función que resta el segundo número del primero.
 const subtract = (a, b) => {
     return a - b;
 };
 
+// Función que multiplica dos números.
 const multiply = (a, b) => {
     return a * b;
 };
 
+// Función que divide el primer número por el segundo. Lanza un error si el divisor es cero.
 const divide = (a, b) => {
     if (b !== 0) {
         return a / b;
@@ -24,6 +29,7 @@ const divide = (a, b) => {
     }
 };
 
+// Exporta un objeto que contiene todas las funciones definidas en el módulo.
 module.exports = {
     getRandomInt: getRandomInt,
     add: add,
